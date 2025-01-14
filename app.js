9e21029f8698dd935c65d95f9543842cdb6c33f9
mfguerrero <mfguerrero@asesoftware.com>
2025-01-13 22:39:08 -0500

    Cambio Import

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const { crearArchivoTabla } = require('./helpers/multiplicar');
-const argv = require('./config/yargs');
-require('colors');
+import { crearArchivoTabla } from './helpers/multiplicar.js';
+import pkg from './config/yargs.js';
+import 'colors';
 
 console.clear();
 
 /* const [, , arg3 = 'base=5'] = process.argv;
 const [, base] = arg3.split('='); */
 
-crearArchivoTabla(argv.b, argv.l, argv.h)
+crearArchivoTabla(pkg.b, pkg.l, pkg.h)
   .then((nombreArchivo) => console.log(nombreArchivo.rainbow, 'creado'.america))
   .catch((err) => console.log(err));
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,14 +1,20 @@
-const fs = require('fs');
-const colors = require('colors');
+import colors from 'colors';
+import fs from 'fs';
 
-const crearArchivoTabla = async (base = 5, listar = false, hasta = 1) => {
+export const crearArchivoTabla = async (
+  base = 5,
+  listar = false,
+  hasta = 1
+) => {
   try {
     let salida = '';
     let consola = '';
 
     for (let i = 1; i <= hasta; i++) {
       salida += `${base} x ${i} = ${base * i}\n`;
-      consola += `${colors.yellow(base)} ${colors.blue('x')} ${colors.red(i)} ${colors.blue('=')} ${colors.magenta(base * i)}\n`;
+      consola += `${colors.yellow(base)} ${colors.blue('x')} ${colors.red(
+        i
+      )} ${colors.blue('=')} ${colors.magenta(base * i)}\n`;
     }
 
     if (listar) {
@@ -25,5 +31,3 @@ const crearArchivoTabla = async (base = 5, listar = false, hasta = 1) => {
     throw err;
   }
 };
-
-module.exports = { crearArchivoTabla };
